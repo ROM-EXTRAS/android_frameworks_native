99bd46257125bcdf17bfbf8503ae832008684e65
Satoshi Sanno <satoshi.sanno@sony.com>
2017-08-10 17:34:02 +0900

    Translate pointer motion events for OneHandOperation Display Shrink
    This patch is a part of the mechanism of display shrink.
    In one handed mode, it translates pointer motion events into a position
    that considers shrinking.
    And if the user touches the out of shrunk display, it notifies to
    InputDispatcherPolicy.
    
    Bug: 112870197
    Change-Id: I1d7882379daa67c4fd38a6326d7c8cfe204f09e7

--- services/inputflinger/dispatcher/include/InputDispatcherPolicyInterface.h
+++ services/inputflinger/dispatcher/include/InputDispatcherPolicyInterface.h
@@ -116,6 +116,8 @@ public:
      * The touchedToken passed as an argument is the window that received the input event.
      */
     virtual void onPointerDownOutsideFocus(const sp<IBinder>& touchedToken) = 0;
+
+    virtual void notifyOutSideScreenTouch(int x, int y) { /* No op by default */ }
 };
 
 } // namespace android
